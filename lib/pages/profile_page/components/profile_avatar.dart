a321ea739476b06a9ebd3c5f38dfe504b414cd79
olegpreed <leo.silveri@mail.ru>
2025-10-01 18:18:13 +0700

    fix: rounded corners for zoomed in avatar

--- lib/pages/profile_page/components/profile_avatar.dart
+++ lib/pages/profile_page/components/profile_avatar.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:forty_two_planet/components/default_avatar.dart';
+import 'package:forty_two_planet/utils/ui_uitls.dart';
 import 'package:fullscreen_image_viewer/fullscreen_image_viewer.dart';
 import 'package:transparent_image/transparent_image.dart';
 
@@ -16,23 +17,35 @@ class ProfileAvatar extends StatelessWidget {
         ? isHomeView
             ? FullscreenViewerWrapper(
                 heroTag: imageUrl!,
-                child: CachedNetworkImage(
-                  imageUrl: imageUrl!,
-                  fit: BoxFit.cover,
-                  placeholder: (context, url) => Container(),
-                  errorWidget: (context, url, error) => const DefaultAvatar(),
-                  fadeInDuration: const Duration(milliseconds: 300),
+                child: Container(
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(Layout.cellWidth / 4),
+                  ),
+                  child: CachedNetworkImage(
+                    imageUrl: imageUrl!,
+                    fit: BoxFit.cover,
+                    placeholder: (context, url) => Container(),
+                    errorWidget: (context, url, error) => const DefaultAvatar(),
+                    fadeInDuration: const Duration(milliseconds: 300),
+                  ),
                 ),
               )
             : FullscreenViewerWrapper(
                 heroTag: imageUrl!,
-                child: FadeInImage.memoryNetwork(
-                  placeholder: kTransparentImage,
-                  image: imageUrl!,
-                  fit: BoxFit.cover,
-                  imageErrorBuilder: (context, error, stackTrace) {
-                    return const DefaultAvatar();
-                  },
+                child: Container(
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(Layout.cellWidth / 4),
+                  ),
+                  child: FadeInImage.memoryNetwork(
+                    placeholder: kTransparentImage,
+                    image: imageUrl!,
+                    fit: BoxFit.cover,
+                    imageErrorBuilder: (context, error, stackTrace) {
+                      return const DefaultAvatar();
+                    },
+                  ),
                 ),
               )
         : const DefaultAvatar();
